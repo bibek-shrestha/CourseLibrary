3b5d3d44d3f983484b2402d6dc27cd737b0bf109
Bibek Shrestha <rawseek@gmail.com>
2024-07-24 17:04:05 +1000

    Initial commit Fix API to standards

---
+++ CourseLibrary.API/Models/AuthorCreationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseLibrary.API.Models;
+
+public class AuthorCreationDto
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public DateTimeOffset DateOfBirth { get; set; }
+
+    public string MainCategory { get; set; } = string.Empty;
+
+}
