942e457611acfd746aa1aa79447dd3ebe709c416
Bibek Shrestha <rawseek@gmail.com>
2024-07-25 15:32:07 +1000

    Use class level custom validation attribute.

--- CourseLibrary.API/Models/CourseManipulationDto.cs
+++ CourseLibrary.API/Models/CourseManipulationDto.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
 
 namespace CourseLibrary.API.Models;
 
-public abstract class CourseManipulationDto: IValidatableObject
+[DifferentTitleAndDescriptionRequired]
+public abstract class CourseManipulationDto
 {
     [Required(ErrorMessage = "Title is required for the course.")]
     [MaxLength(100, ErrorMessage = "Title should not have more than 100 characters.")]
@@ -11,14 +13,4 @@ public abstract class CourseManipulationDto: IValidatableObject
     [MaxLength(1500, ErrorMessage = "Description should not exceed more than 1500 characters")]
     public virtual string Description { get; set; } = string.Empty;
 
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-        if (Title == Description)
-        {
-            yield return new ValidationResult(
-                "Title should not be the same as description for the course.",
-                new [] { "Course" }
-            );
-        }
-    }
 }
---
+++ CourseLibrary.API/ValidationAttributes/DifferentTitleAndDescriptionRequired.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.Models;
+
+namespace CourseLibrary.API.ValidationAttributes;
+
+public class DifferentTitleAndDescriptionRequired: ValidationAttribute
+{
+    public DifferentTitleAndDescriptionRequired()
+    {}
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (validationContext.ObjectInstance is not CourseManipulationDto course)
+        {
+            throw new Exception($"Attribute {nameof(DifferentTitleAndDescriptionRequired)} must be applied to a {nameof(CourseManipulationDto)} or its derived types.");
+        }
+        if (course.Title == course.Description)
+        {
+            return new ValidationResult("The description of the course should not be the same as title of the course."
+            , new[] {"Course"});
+        }
+        return ValidationResult.Success;
+    }
+
+}
